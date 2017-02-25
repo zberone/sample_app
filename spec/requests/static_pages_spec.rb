bfd00a2b830af9ff67ab2733dd0b6db54d9fe388
zberone <zhunumberone@hotmail.com>
2017-02-26 02:11:45 +0800

    first commit

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+describe "Static pages" do
+	let(:base_title) {"ROR Samele App"}
+	describe "Home page" do
+		it "should have the content 'Sample App'" do
+			visit '/static_pages/home'
+			expect(page).to have_content('Sample App')
+		end
+	end
+	describe "Help page" do
+		it "should have the content 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_content('Help')
+		end
+	end
+	describe "About page" do
+		it "should have the content 'About Us'" do
+			visit '/static_pages/about'
+			expect(page).to have_content('About Us')
+		end
+	end
+	describe "Title" do
+		it "should have the right title" do
+			visit '/static_pages/home'
+			expect(page).to have_title("#{base_title} | Home")
+		end
+	end
+	describe "Title" do
+		it "should have the right title" do
+			visit '/static_pages/help'
+			expect(page).to have_title("#{base_title} | Help")
+		end
+	end
+	describe "Title" do
+		it "should have the right title" do
+			visit '/static_pages/about'
+			expect(page).to have_title("#{base_title} | About")
+		end
+	end
+	describe "Contact" do
+		it "should have the content 'Contact Us'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact Us')
+		end
+	end
+	describe "Contact" do
+		it "should have the right title" do
+			visit '/static_pages/contact'
+			expect(page).to have_title("#{base_title} | Contact Us")
+		end
+	end
+end
