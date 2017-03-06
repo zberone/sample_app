5936e9af5776ff61d4f8aad1f85961998d65c690
zberone <zhunumberone@hotmail.com>
2017-03-07 00:37:06 +0800

    add sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
   # get "static_pages/home"
   # get "static_pages/help"
   # get "static_pages/about"
@@ -8,6 +9,7 @@ SampleApp::Application.routes.draw do
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/signup', to: 'users#new', via: 'get'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -41,4 +41,19 @@ describe "Static pages" do
 			expect(page).to have_title("ROR Sample App")
 		end
 	end
+
+	it "should have the right links on the layout" do
+		visit '/'
+		click_link "About"
+		expect(page).to have_title(full_title('About Us'))
+		click_link "Help"
+		expect(page).to have_title(full_title('Help'))
+		click_link "Contact"
+		expect(page).to have_title(full_title('About Us'))
+		click_link "Home"
+		click_link "Sign up now!"
+		expect(page).to have_title(full_title('Sign up'))
+		# click_link "sample app"
+		# expect(page).to have_title(full_title('#'))
+	end
 end
