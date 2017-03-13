6f8ce1322e49c61c0471ddd8523239e4abb13011
zberone <zhunumberone@hotmail.com>
2017-03-13 23:15:41 +0800

    create new user OK

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,7 @@
 module UsersHelper
 	def gravatar_for(user)
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-		image_tag("#{gravatar_id}.png", alt:user.name, class: "gravatar")
+#		image_tag("#{gravatar_id}.png", alt:user.name, class: "gravatar")
+		image_tag("fox.png", alt:user.name, class: "gravatar")
 	end
 end
