d28cb9b1f3591271cbca1bd7f09331d6d20904b7
zberone <zhunumberone@hotmail.com>
2017-03-17 23:42:29 +0800

    add edit&update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
-  def new
+  def new #user regester
   	@user =User.new
   end
 
-  def show
+  def show #show user's information
   	@user = User.find(params[:id])
   end
 
-  def create
+  def create #create new user
   	@user = User.new(user_params) # Not the final implementation!
   	if @user.save
   		# Handle a successful save.
@@ -18,6 +18,22 @@ class UsersController < ApplicationController
   		render 'new'
   	end
   end
+
+  def edit  #edit user information.
+    @user = User.find(params[:id])
+  end
+  def update #update user information
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      #Handle a successful update
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end    
+  end
+
 
   private
   	def user_params
