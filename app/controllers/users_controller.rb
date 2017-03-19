34a7fbf88df7ad44bfd1a9acf154497a54a4362f
zberone <zhunumberone@hotmail.com>
2017-03-20 00:14:45 +0800

    add edit&update to protect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only:[:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def new #user regester
   	@user =User.new
   end
@@ -20,7 +23,7 @@ class UsersController < ApplicationController
   end
 
   def edit  #edit user information.
-    @user = User.find(params[:id])
+#    @user = User.find(params[:id])
   end
   def update #update user information
     @user = User.find(params[:id])
@@ -40,5 +43,17 @@ class UsersController < ApplicationController
   		params.require(:user).permit(:name, :email, :password, 
   										:password_confirmation)
   	end
+
+    # before filters
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 
 end
